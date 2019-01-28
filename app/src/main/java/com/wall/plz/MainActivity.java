1215a4baed16d6f49a16433a87e9b8e34683bd00
Alex <du.alexcruz@gmail.com>
2019-01-28 11:52:14 -0600

    Add lockscreen support
    - Lower sdk down to 24 (nougat)

--- app/src/main/java/com/wall/plz/MainActivity.java
+++ app/src/main/java/com/wall/plz/MainActivity.java
@@ -5,20 +5,25 @@ import android.annotation.SuppressLint;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -26,8 +31,14 @@ import java.io.ByteArrayOutputStream;
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_WRITE_STORAGE_PERMISSION = 1;
-    private ImageView defaultWallpaper;
+    private boolean wallSwitch = false;
 
+    private ImageView defaultWallpaper;
+    private RelativeLayout homeLockPopup;
+    private ParcelFileDescriptor homescreen;
+    private ParcelFileDescriptor lockscreen;
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,18 +46,49 @@ public class MainActivity extends AppCompatActivity {
 
         initPermission();
 
-        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-        Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+        defaultWallpaper = findViewById(R.id.imageView);
+        homeLockPopup = findViewById(R.id.popup);
 
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+
+        homescreen = wallpaperManager.getWallpaperFile(WallpaperManager.FLAG_SYSTEM);
+        lockscreen = wallpaperManager.getWallpaperFile(WallpaperManager.FLAG_LOCK);
+
+        Drawable wallpaperDrawable = wallpaperManager.getDrawable();
         defaultWallpaper = findViewById(R.id.imageView);
         defaultWallpaper.setImageDrawable(wallpaperDrawable);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                customSnackbar(view);
+        defaultWallpaper.setOnLongClickListener(v -> {
+            if (wallSwitch) {
+                if (homescreen != null) {
+                    Bitmap bitmap = BitmapFactory.decodeFileDescriptor(homescreen.getFileDescriptor());
+                    ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                }
+                wallSwitch = false;
+            } else {
+                if (lockscreen != null) {
+                    Bitmap bitmap = BitmapFactory.decodeFileDescriptor(lockscreen.getFileDescriptor());
+                    ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+                }
+                wallSwitch = true;
             }
+            SharedPreferences prefs = getSharedPreferences(
+                    "sharedPreferences", Context.MODE_PRIVATE);
+            prefs.edit().putBoolean("FirstBoot", false).apply();
+            homeLockPopup.setVisibility(View.GONE);
+            return true;
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(this::customSnackbar);
+
+        SharedPreferences prefs = getSharedPreferences("sharedPreferences", Context.MODE_PRIVATE);
+
+        homeLockPopup.setVisibility(prefs.getBoolean("FirstBoot",
+                true) ? View.VISIBLE : View.INVISIBLE);
+        homeLockPopup.setOnClickListener(v -> {
+            prefs.edit().putBoolean("FirstBoot", false).apply();
+            homeLockPopup.setVisibility(View.GONE);
         });
     }
 
@@ -76,24 +118,19 @@ public class MainActivity extends AppCompatActivity {
                 R.layout.custom_snackbar, null);
 
         TextView textViewOne = snackView.findViewById(R.id.txtOne);
-        textViewOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap bitmap = ((BitmapDrawable) defaultWallpaper.getDrawable()).getBitmap();
-                wallpaper(getApplicationContext(), bitmap, true);
-                snackbar.dismiss();
-            }
+        textViewOne.setOnClickListener(v -> {
+            Bitmap bitmap = ((BitmapDrawable) defaultWallpaper.getDrawable()).getBitmap();
+            wallpaper(getApplicationContext(), bitmap, true);
+            snackbar.dismiss();
         });
 
         TextView textViewTwo = snackView.findViewById(R.id.txtTwo);
-        textViewTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap bitmap = ((BitmapDrawable) defaultWallpaper.getDrawable()).getBitmap();
-                wallpaper(getApplicationContext(), bitmap, false);
-                snackbar.dismiss();
-                Snackbar.make(view, R.string.snackbar_saved_wallpaper, Snackbar.LENGTH_SHORT).show();
-            }
+        textViewTwo.setOnClickListener(v -> {
+            Bitmap bitmap = ((BitmapDrawable) defaultWallpaper.getDrawable()).getBitmap();
+            wallpaper(getApplicationContext(), bitmap, false);
+            snackbar.dismiss();
+            Snackbar.make(view, wallSwitch ? R.string.snackbar_hs_saved_wallpaper
+                    : R.string.snackbar_ls_saved_wallpaper, Snackbar.LENGTH_SHORT).show();
         });
 
         layout.addView(snackView, objLayoutParams);
@@ -107,6 +144,8 @@ public class MainActivity extends AppCompatActivity {
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_WRITE_STORAGE_PERMISSION);
+            ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
         }
     }
 }
